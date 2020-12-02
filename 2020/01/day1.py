b9156b9c7559385f39c8bd6a3793c222b65bc286
slangreck <31589520+slangreck@users.noreply.github.com>
2020-12-02 11:56:07 +0100

    Made findSum more generic to be able to find sums consisting of more than two parts

--- 2020/01/day1.py
+++ 2020/01/day1.py
@@ -12,19 +12,29 @@ def main():
     numbers = [int(line) for line in lines]
 
     try:
-        (indexA, indexB) = findSum(numbers, 2020)
+        (indexA, indexB, indexC) = findSum(numbers, 2020, 3)
         print(f'{indexA}: {numbers[indexA]}')
         print(f'{indexB}: {numbers[indexB]}')
-        print(f'Product: {numbers[indexA] * numbers[indexB]}')
+        print(f'{indexC}: {numbers[indexC]}')
+        print(f'Product: {numbers[indexA] * numbers[indexB] * numbers[indexC]}')
     except:
         print('Could not find a solution')
 
 
-def findSum(numbers, sum):
+def findSum(numbers, sum, partCount = 2, start = 0):
     for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            if i != j and numbers[i] + numbers[j] == sum:
-                return (i, j)
+        search = sum - numbers[i]
+
+        if (partCount > 2):
+            try:
+                return (i,) + findSum(numbers, search, partCount - 1, i + 1)
+            except:
+                pass
+        else:
+            try:
+                return (i, numbers.index(search, i + 1))
+            except:
+                pass
     
     raise ValueError('sum not found')
 
