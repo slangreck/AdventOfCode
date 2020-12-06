55fd7773827d8dd76d40053431423259254e3a50
slangreck <31589520+slangreck@users.noreply.github.com>
2020-12-06 17:24:44 +0100

    Added solution for part 2 of day 3

--- 2020/03/day3.py
+++ 2020/03/day3.py
@@ -11,16 +11,30 @@ def main():
 
 	map = [[char == '#' for char in line.strip()] for line in lines]
 
-	print(f'Crashed into {goSledding(map)} trees')
+	runs = [
+		(1, 1),
+		(3, 1),
+		(5, 1),
+		(7, 1),
+		(1, 2)
+	]
 
-def goSledding(map):
+	product = 1
+	for run in runs:
+		crashes = goSledding(map, run[0], run[1])
+		print(f'Right {run[0]}, down {run[1]}: {crashes}')
+		product = product * crashes
+
+	print(f'Product: {product}')
+
+def goSledding(map, stepHorizontal, stepVertical):
 	counter = 0
 	horizontal = 0
 
-	for line in map:
+	for line in map[::stepVertical]:
 		if line[horizontal]:
 			counter = counter + 1
-		horizontal = (horizontal + 3) % len(line)
+		horizontal = (horizontal + stepHorizontal) % len(line)
 
 	return counter
 
