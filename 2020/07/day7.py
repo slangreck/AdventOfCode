5bf7c71c74ffce62af343a639440b863d1c1eb99
slangreck <31589520+slangreck@users.noreply.github.com>
2020-12-10 16:37:25 +0100

    Added solution for part 2 of day 7

--- 2020/07/day7.py
+++ 2020/07/day7.py
@@ -12,15 +12,16 @@ def main():
 
     bags = parseRules(rules)
 
-    shinyGoldContainers = findContainers(bags['shiny gold'])
-    print([container.color for container in shinyGoldContainers])
+    shinyGoldBag = bags['shiny gold']
+    shinyGoldContainers = shinyGoldBag.findContainers()
     print(f'Shiny gold bags can be carried in {len(shinyGoldContainers)} other bags')
+    print(f'Shiny gold bags must contain {shinyGoldBag.countContainedBags()} other bags')
 
 def parseRules(rules):
     bags = {bag.color: bag for bag in [Bag(rule) for rule in rules]}
 
     ruleRegex = re.compile(r'(\d+) ([a-z]+ [a-z]+) bags?')
-    for color, bag in bags.items():
+    for bag in bags.values():
         for rule in bag.containsRule.split(', '):
             match = ruleRegex.match(rule)
             if match and match.group(2) in bags:
@@ -29,14 +30,6 @@ def parseRules(rules):
                 cBag.containedIn[bag.color] = bag
 
     return bags
-
-def findContainers(bag, containers = set()):
-    for color, container in bag.containedIn.items():
-        if container not in containers:
-            containers.add(container)
-            findContainers(container, containers)
-
-    return containers
 
 
 class Bag:
@@ -47,9 +40,24 @@ class Bag:
         self.contains = {}
         self.containedIn = {}
 
+    def findContainers(self, containers = set()):
+        for container in self.containedIn.values():
+            if container not in containers:
+                containers.add(container)
+                container.findContainers(containers)
+
+        return containers
+
+    def countContainedBags(self):
+        count = 0
+        for contained in self.contains.values():
+            count += contained.count + contained.count * contained.bag.countContainedBags()
+
+        return count
+
 class ContainedBag:
     def __init__(self, bag, count):
-        self.bag = bag,
+        self.bag = bag
         self.count = count
 
 if __name__ == "__main__":
